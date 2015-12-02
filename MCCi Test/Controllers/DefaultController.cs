ee40d476f61605d718b7b9f8ef5694f703c6f932
Nick Mckloski <Mckloski.Nick@gmail.com>
2015-12-02 00:40:15 -0500

    init commit

---
+++ MCCi Test/Controllers/DefaultController.cs
@@ -0,0 +1,86 @@
+﻿using MCCi_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCCi_Test.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        //list of users
+        public static IList<UserModel> users = new List<UserModel>();
+        
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View(users);
+        }
+
+
+        //new user controller
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+
+        //handle post on new user
+        [HttpPost]
+        public ActionResult NewUser(UserModel user)
+        {
+            //if model is not valid return the view
+            if(!ModelState.IsValid)
+                return View(user);
+
+            //add model to the list and redirect to index
+            users.Add(user);
+            return RedirectToAction("Index");
+        }
+
+
+        //edit user controller
+        public ActionResult EditUser(UserModel user)
+        {
+            //handle post
+            if (Request.HttpMethod == "POST")
+            {
+
+                //clear errors from the unique id model check since that user is being edited
+                ModelState["UserID"].Errors.Clear();
+
+                //if model is not valid return the view
+                if (!ModelState.IsValid)
+                    return View(user);
+
+                //find and set the new values
+                for(int i = 0; i < users.Count(); i++)
+                {
+                    if(users[i].UserID == user.UserID)
+                    {
+                        users[i].Prefix = user.Prefix;
+                        users[i].Suffix = user.Suffix;
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        //delete user controller
+        public ActionResult DeleteUser(UserModel user)
+        {
+            //find and remove the user
+            for (int i = 0; i < users.Count(); i++)
+            {
+                if (users[i].UserID == user.UserID)
+                    users.RemoveAt(i);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MCCi Test/Models/UserModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MCCi_Test.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [DisplayName("Prefix")]
+        public string Prefix { get; set; }
+
+        [Required]
+        [UniqueID]
+        [DisplayName("User ID")]
+        public string UserID { get; set; }
+
+        [Required]
+        [DisplayName("Suffix")]
+        public string Suffix { get; set; }
+
+        public RouteValueDictionary RouteValues
+        {
+            get
+            {
+                var values = new RouteValueDictionary();
+                values["Prefix"] = Prefix;
+                values["UserID"] = UserID;
+                values["Suffix"] = Suffix;
+                return values;
+            }
+        }
+
+    }
+
+    //custom atribute class
+    public class UniqueID : ValidationAttribute
+    {
+        //override validation method to check custom requirement
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var unique = true;
+
+            foreach (var user in MCCi_Test.Controllers.DefaultController.users)
+            {
+                if (user.UserID == value.ToString())
+                    unique = false;
+            }
+
+            if (!unique)
+                return new ValidationResult("This user ID already exists.");
+
+            return ValidationResult.Success;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class IgnoreValidation : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var modelState = filterContext.Controller.ViewData.ModelState;
+
+            foreach (var modelValue in modelState.Values)
+            {
+                modelValue.Errors.Clear();
+            }
+        }
+    }
+
+}
